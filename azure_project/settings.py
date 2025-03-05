c18ab0355b2ee6693b13f4d669b75488ccf218aa
kracst <stefbraescu232@gmail.com>
2025-03-05 11:23:34 +0200

    modified settings

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -108,7 +108,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('azure_content/static')),)
-STATIC_URL = '/azure_content/static/'
+STATIC_URL = '/static/'
 
 
 
